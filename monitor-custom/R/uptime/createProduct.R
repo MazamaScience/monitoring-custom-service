03e5d00e3a6113421f84665516ac54fe57ca61f2
Spencer Pease <spencer.p.816@gmail.com>
2018-05-18 18:42:02 -0700

    Update `createProduct()` to fit into info/data/text framework
    - Also change plotting to ggplot

--- monitor-custom/R/uptime/createProduct.R
+++ monitor-custom/R/uptime/createProduct.R
@@ -1,21 +1,60 @@
-# uptime chart
+########################################################################
+# uptime/createProduct.R
+#
+# Create the a 'tarnay' summary barplot.
+#
+# Author: Spencer Pease, Jonathan Callahan
+########################################################################
 
-createProduct <- function(serverid='tools-c3', ymax=1.5) {
-  
-  # Read in data
-  logUrl <- paste0('https://',serverid,'.airfire.org/logs/uptime.log')
-  df <- readr::read_csv(logUrl, col_names=c('datetime','hms','user','load_1_min','load_5_min','load_15_min'))
+#' @title Create Uptime / load timeseries chart
+#'
+#' @description
+#'
 
-  # Clean up data  
-  df$datetime <- lubridate::ymd_hms( stringr::str_sub(df$datetime, 1, 19) )
-  df$hms <- NULL
-  df$user <- NULL
-  df$load_1_min <- as.numeric( stringr::str_replace(df$load_1_min, "load average: ", "") )
+createProduct <- function(dataList = NULL, infoList = NULL, textList = NULL) {
 
-  # Plot data  
-  plot(df$load_15_min ~ df$datetime, ylim=c(0,ymax), type='s', lwd=2, las=1, xpd=NA)
-  points(df$load_1_min ~ df$datetime, pch=17, cex=df$load_1_min * 1, col='red', xpd=NA)
-  
+  logger.trace("----- createProduct() [uptime] -----")
+
+  if (is.null(dataList)) stop(paste0("Required parameter 'dataList' is missing."), call. = FALSE)
+  if (is.null(infoList)) stop(paste0("Required parameter 'infoList' is missing."), call. = FALSE)
+  if (is.null(textList)) stop(paste0("Required parameter 'textList' is missing."), call. = FALSE)
+
+  # ----- get parameters ------------------------------------------------------
+
+  uptimeData <- dataList$uptimeData
+  serverID <- infoList$serverid
+  yMax <- infoList$ymax
+
+  plotPath <- infoList$plotPath
+  width <- infoList$width
+  height <- infoList$height
+  dpi <- infoList$dpi
+  units <- infoList$units
+
+  # ----- Create plot ---------------------------------------------------------
+
+  uptimePlot <- ggplot(uptimeData, aes(x = datetime)) +
+    geom_step(aes(y = load_15_min)) +
+    ylim(0, min(yMax, max(uptimeData$load_15_min) * 1.25)) +
+    labs(
+      title = paste("Uptime for:", serverID),
+      x = "Time",
+      y = "Load (15 Minute Average)") +
+    ggthemes::theme_hc() +
+    scale_x_datetime(
+      date_labels = "%m/%d %H:%M"
+    )
+
+  # ----- Save plot -----------------------------------------------------------
+
+  ggsave(
+    plotPath,
+    plot = uptimePlot,
+    width = width,
+    height = height,
+    dpi = dpi,
+    units = units
+  )
+
+  return(invisible())
 }
-
-
