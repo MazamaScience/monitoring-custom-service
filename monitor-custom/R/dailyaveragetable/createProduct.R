377aeaa5fe3d1e29a49c49daaecf761ab9d15fd0
Tate Brasel <tatebrasel@gmail.com>
2021-01-26 13:32:34 -0800

    feat: added debug script for dailyaveragetable createProduct

--- monitor-custom/R/dailyaveragetable/createProduct.R
+++ monitor-custom/R/dailyaveragetable/createProduct.R
@@ -214,3 +214,149 @@ createProduct <- function(dataList = NULL, infoList = NULL, textList = NULL) {
 
   return(invisible())
 }
+
+if ( FALSE ) {
+  
+  # Must set working directory to monitor-custom/
+  # Output is placed into monitor-custom/output/
+  
+  req <- list(
+    path = "/monitor-custom/dev/dailyaveragetable",
+    parameters = list(
+      monitorids = "530330030_01,410510080_01,060750005_01",
+      startdate = "20200910",
+      enddate = "20201001"
+    )
+  )
+  
+  if ( TRUE) {
+    
+    # beakr instance configuration
+    BEAKR_HOST <- "127.0.0.1" # beakr default
+    BEAKR_PORT <- "8080"      # beakr default
+    
+    # path and cache
+    SERVICE_PATH <- "monitor-custom/dev"
+    CACHE_SIZE <- 5 # megabytes
+    
+    # directories for log output, data, and cache
+    DATA_DIR <- file.path(getwd(), "data")
+    if ( !file.exists(DATA_DIR) ) dir.create(DATA_DIR)
+    
+    LOG_DIR <- file.path(getwd(), "logs")
+    if ( !file.exists(LOG_DIR) ) dir.create(LOG_DIR)
+    
+    CACHE_DIR <- file.path(getwd(), "output")
+    if ( !file.exists(CACHE_DIR) ) dir.create(CACHE_DIR)
+    
+    # Clean out the cache (only when running from RStudio)
+    removalStatus <- file.remove( list.files(CACHE_DIR, full.names = TRUE) )
+    
+    MazamaCoreUtils::initializeLogging(LOG_DIR)
+    
+    logger.setLevel(TRACE)
+    
+    # Capture session info
+    logger.debug(capture.output(sessionInfo()))
+    
+    # Log environment variables
+    logger.debug('BEAKR_HOST = %s', BEAKR_HOST)
+    logger.debug('BEAKR_PORT = %s', BEAKR_PORT)
+    logger.debug('SERVICE_PATH = %s', SERVICE_PATH)
+    logger.debug('CACHE_DIR = %s', CACHE_DIR)
+    logger.debug('CACHE_SIZE = %s', CACHE_SIZE)
+    logger.debug('DATA_DIR = %s', DATA_DIR)
+    logger.debug('LOG_DIR = %s', LOG_DIR)
+    
+    # Extract lowercase subservice name
+    subservice <-
+      stringr::str_replace(req$path, SERVICE_PATH, "") %>%
+      stringr::str_replace_all("/", "") %>%
+      stringr::str_to_lower()
+    
+    logger.info("----- %s -----", req$path)
+    
+    # Create subservice script paths
+    infoListScript <- paste0("R/", subservice, "/createInfoList.R")
+    dataListScript <- paste0("R/", subservice, "/createDataList.R")
+    productScript <- paste0("R/", subservice, "/createProduct.R")
+    setMonitorIDsScript <- "R/sharedUtils/setMonitorIDs.R"
+    
+    # Source these scripts
+    result <- try({
+      source(infoListScript)        # function to convert request into infoList required by product
+      source(dataListScript)        # function to load data required by product
+      source(productScript)         # function to create product
+      source(setMonitorIDsScript)   # function to set monitor IDs
+    }, silent = TRUE)
+    stopOnError(result)
+    
+    printUTC <- function(x) {
+      strftime(x, format = "%Y-%m-%d %H:%M:%S", tz = "UTC", usetz = TRUE)
+    }
+    
+    # Create infoList
+    result <- try({
+      infoList <- createInfoList(req, CACHE_DIR)
+    }, silent = TRUE)
+    stopOnError(result)
+    
+    # Create a new plot file if it isn't in the cache
+    if ( !file.exists(infoList$plotPath) ) {
+      
+      # Manage the cache
+      MazamaCoreUtils::manageCache(CACHE_DIR, c("json", "png", "pdf", "xlsx")) # TODO:  Other potential output formats?
+      
+      result <- try({
+        
+        # Get data and text for this product
+        dataList <- createDataList(infoList, DATA_DIR)
+        
+        # Get language dependent plot labels
+        textListScript <- paste(
+          "R/", subservice, "/createTextList_", infoList$language, ".R",
+          sep = "")
+        source(textListScript)
+        textList <- createTextList(dataList, infoList)
+        
+        # Create product
+        createProduct(dataList, infoList, textList)
+        
+        logger.info("successfully created %s", infoList$plotPath)
+        
+      }, silent = TRUE)
+      stopOnError(result)
+      
+    } # finished creating product file
+    
+    
+    # Create a new json file if it isn't in the cache
+    if (!file.exists(infoList$jsonPath)) {
+      
+      result <- try({
+        
+        logger.debug("writing %s", infoList$jsonPath)
+        
+        responseList <- list(
+          status = "OK",
+          rel_base = paste0(SERVICE_PATH, "/", infoList$basePath),
+          plot_path = paste0(SERVICE_PATH, "/", infoList$plotPath)
+        )
+        
+        json <- jsonlite::toJSON(
+          responseList,
+          na = "null",
+          pretty = TRUE,
+          auto_unbox = TRUE)
+        write(json, infoList$jsonPath)
+        logger.debug("successfully created %s", infoList$jsonPath)
+        
+      }, silent = TRUE)
+      stopOnError(result)
+      
+    } # finished creating json file
+  
+  }
+  
+}
+
