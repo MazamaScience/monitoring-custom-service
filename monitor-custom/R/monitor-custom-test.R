14c977f0c741b8d8bde150eb3496213694175465
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2021-01-29 14:16:01 -0800

    refactor: adding monitor-custom-test.R

---
+++ monitor-custom/R/monitor-custom-test.R
@@ -0,0 +1,139 @@
+################################################################################
+# monitor-custom-test.R
+#
+# Author: Tate Brasel,
+#         Spencer Pease
+#         Jonathan Callahan <jonathan@mazamscience.com>
+#
+# Test version of the app code that can be sourced in RStudio.
+#
+################################################################################
+
+# Incoming request
+
+subservice <- "dailyhourlybarplot"
+
+req <- list(
+  parameters = list(
+    monitorids = "840TT1820013_01,160690012_01,530030004_01",
+    days = 10
+  )
+)
+
+# ----- Libraries and scripts --------------------------------------------------
+
+# NOTE:  Use library() so that these package versions will be documented by
+# NOTE:  sessionInfo()
+
+suppressPackageStartupMessages({
+  library(readr)             # tidyverse file reading
+  library(digest)            # creation of uniqueID
+  library(flextable)         # creation of table graphics
+  library(openxlsx)          # creation of spreadsheets
+  # Mazama Science packages
+  library(beakr)             # web service framework
+  library(MazamaCoreUtils)   # cache management and more
+  library(PWFSLSmoke)        # workhorse package for everything smoke related
+  library(AirMonitorPlots)   # plotting functions for monitor data
+})
+
+R_files <- list.files("R/sharedUtils", pattern = ".+\\.R", full.names = TRUE)
+
+for (file in R_files) {
+  source(file.path(getwd(), file))
+}
+
+# Specify global (configurable) variables --------------------------------------
+
+# V4 data files . dailyaveragetable . xlsx improvements; csv output
+VERSION <- "4.5.1"
+
+# Set up configurable variables
+
+# directories for log output, data, and cache
+DATA_DIR <- file.path(getwd(), "data")
+if ( !file.exists(DATA_DIR) ) dir.create(DATA_DIR)
+
+LOG_DIR <- file.path(getwd(), "logs")
+if ( !file.exists(LOG_DIR) ) dir.create(LOG_DIR)
+
+CACHE_DIR <- file.path(getwd(), "output")
+if ( !file.exists(CACHE_DIR) ) dir.create(CACHE_DIR)
+
+# Clean out the cache (only when running from RStudio)
+removalStatus <- file.remove( list.files(CACHE_DIR, full.names = TRUE) )
+
+# Silence other warning messages
+options(warn = -1) # -1=ignore, 0=save/print, 1=print, 2=error
+
+
+# ----- Set up Logging ---------------------------------------------------------
+
+MazamaCoreUtils::initializeLogging(LOG_DIR)
+
+logger.setLevel(TRACE)
+
+# Capture session info
+logger.debug(capture.output(sessionInfo()))
+
+# Log environment variables
+logger.debug('CACHE_DIR = %s', CACHE_DIR)
+logger.debug('DATA_DIR = %s', DATA_DIR)
+logger.debug('LOG_DIR = %s', LOG_DIR)
+
+
+# ----- Define helper functions ------------------------------------------------
+
+printUTC <- function(x) {
+  strftime(x, format = "%Y-%m-%d %H:%M:%S", tz = "UTC", usetz = TRUE)
+}
+
+# * Create Products ----------------------------------------------------------
+
+# NOTE:  All subservices are handled the same way. Each has its own subdirectory
+# NOTE:  with files that define the following top-level functions:
+# NOTE:   * createDataList
+# NOTE:   * createInfoList
+# NOTE:   * createProduct
+# NOTE:   * createTextList
+# NOTE:
+# NOTE:  This standard protocol allows the following chunk of code to be
+# NOTE:  run for every custom product.
+
+
+# Create subservice script paths
+infoListScript <- paste0("R/", subservice, "/createInfoList.R")
+dataListScript <- paste0("R/", subservice, "/createDataList.R")
+productScript <- paste0("R/", subservice, "/createProduct.R")
+
+# Source these scripts
+result <- try({
+  source(infoListScript)        # function to convert request into infoList required by product
+  source(dataListScript)        # function to load data required by product
+  source(productScript)         # function to create product
+}, silent = TRUE)
+stopOnError(result)
+
+# Create infoList
+infoList <- createInfoList(req, CACHE_DIR)
+
+# Create a new plot file if it isn't in the cache
+
+# Get data and text for this product
+dataList <- createDataList(infoList, DATA_DIR)
+
+# Get language dependent plot labels
+textListScript <- paste(
+  "R/", subservice, "/createTextList_", infoList$language, ".R",
+  sep = "")
+source(textListScript)
+textList <- createTextList(dataList, infoList)
+
+# Create product
+createProduct(dataList, infoList, textList)
+
+logger.info("successfully created %s", infoList$plotPath)
+
+
+
+
